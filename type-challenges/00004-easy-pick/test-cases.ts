c076ef441c60e2b2129130f370dfde3101cbf6d6
褚涸辙 <chuhezhe0807@163.com>
2023-04-15 15:11:34 +0800

    others: Add tsconfig.json

--- type-challenges/00004-easy-pick/test-cases.ts
+++ type-challenges/00004-easy-pick/test-cases.ts
@@ -1,23 +1,44 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
-  Expect<Equal<Expected1, MyPick<Todo, 'title'>>>,
-  Expect<Equal<Expected2, MyPick<Todo, 'title' | 'completed'>>>,
-  // @ts-expect-error
-  MyPick<Todo, 'title' | 'completed' | 'invalid'>,
+    Expect<Equal<Expected1, MyPick<Todo, 'title'>>>,
+    Expect<Equal<Expected2, MyPick<Todo, 'title' | 'completed'>>>,
+    // @ts-expect-error
+    MyPick<Todo, 'title' | 'completed' | 'invalid'>,
 ]
 
 interface Todo {
-  title: string
-  description: string
-  completed: boolean
+    title: string
+    description: string
+    completed: boolean
 }
 
 interface Expected1 {
-  title: string
+    title: string
 }
 
 interface Expected2 {
-  title: string
-  completed: boolean
+    title: string
+    completed: boolean
+}
+
+type MyPick<T, K extends keyof T> = {
+    [P in K]: T[P]
+}
+
+interface IObj {
+    name: string;
+    age: number;
+}
+
+const obj: IObj = {
+    name: "xiaozhang",
+    age: 20
+}
+
+function func(abc: MyPick<IObj, "age">): MyPick<IObj, "name"> {
+    console.log(abc.age);
+
+
+    return { name: "111" }
 }
